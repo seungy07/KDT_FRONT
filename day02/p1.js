480f22462b3b16f29079f0205a87e158b4ff3908
seungy07 <ksykim07@gmail.com>
2026-07-07 18:14:33 +0900

    07/07_수업 끝

---
+++ day02/p1.js
@@ -0,0 +1,17 @@
+let 성명 = prompt()
+let 메뉴 =  prompt()
+let 수량 = prompt()
+
+const 주문 = "[주문 접수]"
+console.log(주문)
+console.log("이름")
+console.log(성명)
+
+const 메뉴이름 = "메뉴"
+console.log(메뉴이름)
+console.log(메뉴)
+
+const 잔 = "수량"
+console.log(잔)
+console.log(수량)
+
---
+++ day02/p2.js
@@ -0,0 +1,4 @@
+let 이름 = prompt()
+let 운동종류 = prompt()
+let 운동시간 = prompt()
+

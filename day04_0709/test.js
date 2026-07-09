821730dbb574523dc7fe0999cf9090b702767e4a
seungy07 <ksykim07@gmail.com>
2026-07-09 15:35:54 +0900

    찐찐찐수정

--- day04_0709/test.js
+++ day04_0709/test.js
@@ -1,11 +1,12 @@
 let gamestart = 1
+let board = ["","","","","","","","",""]
 
-let player1 = Number(prompt("플레이어1 차례")); let p1_board = ["","","","","","","","",""]
 const 승리조건 = 
 [[0, 1, 2], [3, 4, 5], [6, 7, 8],    // 가로 승리조건
 [0, 3, 6], [1, 4, 7], [2, 5, 8],     // 세로 승리조건
 [0, 4, 8], [2, 4, 6]]                //대각선 승리 조건
-let board = ["","","","","","","","",""]
+
+let player1 = Number(prompt("플레이어1 차례")); let p1_board = ["","","","","","","","",""]
 
 if(player1 == 0 || player1 == 1 || player1 == 2 || player1 == 3 || player1 == 4 || player1 == 5 || player1 == 6 || player1 == 7|| player1 == 8 )
     {console.log(`${board[player1]='O'}`); console.log(board); p1_board.push(player1); 

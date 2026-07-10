a136344d1530b425def5e4d311b6f9af18668883
seungy07 <ksykim07@gmail.com>
2026-07-10 11:08:06 +0900

    교수님 풀이 반영

--- day04_0709/p3.js
+++ day04_0709/p3.js
@@ -1,5 +1,4 @@
 // 문제1: 과일 목록 관리
-
 let fruitList = [ '사과', '바나나']
 let 과일 = prompt('과일 입력')
 let 존재여부 = fruitList.includes(과일)
@@ -33,6 +32,14 @@ let c = Number(prompt("서로다른 수 중 세번째 수 입력"))
 if(a>b && a>c){console.log("가장큰수는 첫번쨰 수")}
 if(b>a && b>c){console.log("가장큰수는 두번쨰 수")}
 if(c>b && c>a){console.log("가장큰수는 세번쨰 수")}
+/* 교수님 풀이
+let max = a  첫번째 값을 ㄱ장 큰 수 시작
+//두번쨰 값이 max 보다 큰지 확인 , if~else if 가 아닌 if~ if
+// 스왑 (두 변수간 값 교체) * 변수특징: 하나의 값 저장 
+if( max < b){ max = b}
+if( max < c){ max = c}
+console.log( max ) 
+*/
 
 //문제6. 윤년 판별기
 let 연도 = Number(prompt('년도 입력'))
@@ -49,14 +56,29 @@ else if(수2<수1 && 수2<수3 && 수1<수3){console.log(`${수2} ${수1} ${수3
 else if(수2<수1 && 수2<수3 && 수3<수1){console.log(`${수2} ${수3} ${수1}`)}
 else if(수3<수2 && 수3<수1 && 수2<수1){console.log(`${수3} ${수2} ${수1}`)}
 else if(수3<수1 && 수3<수2 && 수1<수2){console.log(`${수2} ${수1} ${수3}`)}
-
+/* 풀이 17 , 4, 8
+수1 -> 수2 비교  더 크면 자리 바꾸기(스왑)
+수1 -> 수3 비교 더 크면 자리 바꾸기
+수 마다 두번씩 비교
+if( 수1 > 수2 ){  //오름차순이 아닌 내림차순이면 부호만 반대로
+    let temp = 수1 // 두 변수간의 값 스왑 또는 교체를 하기 위한 임시 저장소
+    수1 = 수2    // 수1 = 4   ,  스왑하기위해 임시 변수가 필요
+    수2 = temp;   // 수2 = 17
+}
+if( 수1 > 수3 ){ let temp = 수1; 수1=수3; 수3=temp; }
+if( 수2 > 수3 ){ let temp = 수2; 수2=수3; 수3=temp; }
+console.log(`${수1}${수2}${수3}`)
+*/
 //문제8: 가위바위보 게임
 let p1 = Number(prompt('플레이어 1 선택0(가위) 1(바위) 2(보)'))
 let p2 = Number(prompt('플레이어 2 선택 0(가위) 1(바위) 2(보)'))
 if(p1 == p2){console.log("무승부")}
-else if((p1 == 0 && p2 == 2) || (p1 ==1 && p2 ==0) || (p1 ==2 && p2 ==1)){console.log("플레이어1 승리")} //1
+else if((p1 == 0 && p2 == 2) || (p1 ==1 && p2 ==0) || (p1 ==2 && p2 ==1)){console.log("플레이어1 승리")}
 else{console.log("플레이어2 승리")}
-
+/* 풀이
+p1 == (p2 + 1) % 3 이용
+if( p1 == (p2 + 1) % 3){console.log('p1이김')}
+*/
 //문제9: 주차 차량 위치 검색
 let carArray = ['250어7142', '142가7415', '888호8888']
 let locationArray = [ 'A1', 'B3', 'C2']

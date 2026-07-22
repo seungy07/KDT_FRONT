4b428b3b5ed48bbc3cfdb6954f611c638de6d14e
seungy07 <ksykim07@gmail.com>
2026-07-22 09:15:58 +0900

    js_p7 풀이

---
+++ day11_0721/js_p7.js
@@ -0,0 +1,21 @@
+function 등록(){
+    // 날짜 
+    let 날짜 = document.querySelector(".date")
+    let 날짜값 = 날짜.value
+
+    //항목이름
+    let 항목 = document.querySelector(".항목")
+    let 항목값 = 항목.value.toLocaleString()
+
+    //금액
+    let 금액 = document.querySelector(".금액")  
+    let 금액값 = 금액.value
+
+    //tbody 마크업 사이에 값을 넣는다
+    let tbody = document.querySelector('tbody')
+    tbody.innerHTML += `<tr> 
+                        <td> ${날짜값} </td>
+                        <td> ${항목값} </td>
+                        <td> ${금액값} </td>
+                       </tr>`
+}

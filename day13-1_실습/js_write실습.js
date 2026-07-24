82a4c53a409591125853984625c2ecdc0ea75cd4
seungy07 <ksykim07@gmail.com>
2026-07-24 10:18:45 +0900

    피드백 반영

--- day13-1_실습/js_write실습.js
+++ day13-1_실습/js_write실습.js
@@ -3,11 +3,11 @@ function boardWrite(){
     let title =document.querySelector('#titleInput').value
     let content =document.querySelector('#contentInput').value
     let pw =document.querySelector('#pwdInput').value
+    // 중간중간 console.log()로 값을 잘 출력/반환 하는지 체크
     let object = {title, content, pw}
 
     let boardList = JSON.parse(localStorage.getItem('boardList'))
-    if(boardList == null){ boardList = []}
-
+    if(boardList == null){ boardList = [] }
     
     object.no = boardList.length == 0 ? 1: boardList[boardList.length-1].no+1
     boardList.push(object)
